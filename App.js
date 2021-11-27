13689db3d149fd3ff34c6aa2d3365bb52ff473f8
Jaka Dwi Prasetyo <pelolong24@gmail.com>
2021-11-27 23:08:58 +0700

    Profile Instagram Styling
    Profile Instagram

--- App.js
+++ App.js
@@ -1,55 +1,130 @@
 import React, { Component } from "react";
-import { View, Text, Image, TextInput, StyleSheet} from "react-native";
-import foto from './Homepage.jpg';
+import { View, Text, Image, Button} from "react-native";
+import tambah from './img/tambah.png';
+import list from './img/list.png';
+import kotak from './img/kotak.png';
+import vid from './img/vid.png';
+import tag from './img/tag.png';
+import account from './img/account.png';
+import jaka from './img/jaka.png';
+import pia from './img/pia.png';
+import kucing from './img/kucing.png';
+import plus from './img/plus.png';
+import foto1 from './img/foto1.png';
+import foto2 from './img/foto2.png';
+import foto3 from './img/foto3.png';
+import home from './img/home.png';
+import search from './img/search.png';
+import reels from './img/reels.png';
+import shop from './img/shop.png';
 import { style } from "./style";
 
 const App = () => {
   return (
     <View style={style.container}>
-    <Photo/>
-    <Teks/>
-    <TextInput style={style.masukan}>Add a comment...</TextInput>
-    {/* <Text style={style.tulisanku}>Selamat Datang</Text> */}
-    {/* <Image source={{uri:'https://placeimg.com/100/100/nature'}} style={{width:100, height:100}}></Image> */}
-    {/* <Tampilan/> */}
-    {/* <Tulisan/> */}
-    {/* <Photoku/> */}
+      <View style={{flex:1, backgroundColor:'white', flexDirection:'row'}}>
+        <View style={{flex:6, backgroundColor:'white',
+          justifyContent:'center'}}>
+            <Text style={{fontSize:19, color:'black'}}>Jaka Dwi Prasetyo</Text>
+        </View>
+        <TampilanGambar simbol={tambah} />
+        <TampilanGambar simbol={list} />
+      </View>
+      <View style={{flex:2, backgroundColor:'white',
+        flexDirection:'row'}}>
+          <View style={style.viewGambar}>
+            <Image source={account} style={{width:90,
+              height:90, borderRadius:90/2}} />
+      </View>
+          <CardProfile angka="100" keterangan="Post" />
+          <CardProfile angka="88.4M" keterangan="Followers" />
+          <CardProfile angka="2,110" keterangan="Following" />
+      </View>
+      <View style={{flex:1.5, backgroundColor:'white'}}>
+        <Text style={{fontSize:15, color:'black'}}>Biasa di panggil Jakk</Text>
+        <Text style={{fontSize:13}}>Followers Jutaan Tapi Gadapet Centang Biru</Text>
+        <Text style={style.textBio}>@piapio</Text>
+      </View>
+      
+      <View style={{flex:1, backgroundColor:'white', flexDirection:'row'}}>
+          <View style={{flex:7, backgroundColor:'white'}}>
+            <Button title="Edit Profil" ></Button>
+          </View>
+          <View style={{flex:1, backgroundColor:'white'}}>
+            <Button title="v" style={{borderRadius:5}} ></Button>
+          </View>
+      </View>
+      <View style={{flex:2, backgroundColor:'white', flexDirection:'row'}}>
+        <Sorotan gambar={jaka} keteranganSorotan="Si Ganteng" />
+        <Sorotan gambar={pia} keteranganSorotan="Si Cantik" />
+        <Sorotan gambar={kucing} keteranganSorotan="Si Majikan" />
+        <Sorotan gambar={plus} keteranganSorotan="New" />
+      </View>
+        <View style={{flex:1, backgroundColor:'white', flexDirection:'row'}}>
+          <TampilanGambar simbol={kotak} />
+          <TampilanGambar simbol={vid} />
+          <TampilanGambar simbol={tag} /> 
+      </View>
+      <View style={{flex:1, backgroundColor:'white', flexDirection:'row'}}>
+        <Foto feed={foto1} />
+        <Foto feed={foto2} />
+        <Foto feed={foto3} />
+      </View>
+      <View style={{flex:1, backgroundColor:'white', flexDirection:"row"}}>
+        <Navigasi nav={home} />
+        <Navigasi nav={search} />
+        <Navigasi nav={reels} />
+        <Navigasi nav={shop} />
+        <View style={style.viewGambar}>
+            <Image source={account} style={{width:28,
+              height:28, borderRadius:80/2}} />
+        </View>
+      </View>
+    </View>     
+  )
+}
+
+const TampilanGambar = (props) => {
+  return (
+    <View style={style.viewGambar}>
+      <Image source={props.simbol} style={style.gambar} />
     </View>
-  );
-}
-
-const Teks = () => {
-  return (
-    <Text style={style.teks}>4,000 likes</Text>
   )
 }
 
-const Photo = () => {
+const CardProfile = (props) => {
   return (
-    <Image source={foto} style={{width:320, height:510}}/>
+    <View style={style.viewGambar}>
+      <Text style={style.textProfil}>{props.angka}</Text>
+      <Text style={style.keteranganProfil}>
+        {props.keterangan}</Text>
+    </View>
   )
 }
 
-// const Tampilan = () => {
-//   return (
-//     <View style={{width:100, height:100, backgroundColor:'green'}}/>
-//   )
-// }
+const Sorotan = (props) => {
+  return (
+    <View style={style.viewGambar}>
+      <Image source={props.gambar} style=
+      {style.gambarProfil} />
+      <Text>{props.keteranganSorotan}</Text>
+    </View>
+  )
+}
 
-// class Tulisan extends Component {
-//   render () {
-//     return (
-//         <Text>Tampilan dari komponen class</Text>
-//     )
-//   }
-// }
+const Foto = (props) => {
+  return (
+    <View style={style.viewRow}>
+      <Image source={props.feed} style={style.gridGambar} />
+    </View>
+  )
+}
 
-// class Photoku extends Component {
-//   render () {
-//     return (
-//       <Image source={{uri:'https://placeimg.com/100/100/tech'}} style={{width:200, height:200}}/>
-//     )
-//   }
-// }
-
+const Navigasi = (props) => {
+  return (
+    <View style={style.viewGambar}>
+      <Image source={props.nav} style={style.gambar} />
+    </View>
+  )
+}
 export default App;
--- style.js
+++ style.js
@@ -3,25 +3,47 @@ import { StyleSheet } from "react-native";
 const style=StyleSheet.create({
     container: {
       flex:1,
-      padding:13,
-      paddingTop:0,
-      margin:5,
-    },
-    teks: {
-      marginTop:0,
-      paddingVertical:0,
-      paddingLeft:5,
-      borderWidth:0,
-      fontSize:12
-    },
-    masukan: {
-        marginTop:0,
-        paddingVertical:0,
-        paddingLeft:5,
-        borderWidth:0,
-        fontSize:14
-    }
-  });
-
+      backgroundColor:'white',
+      margin:10
+      },
+      gambar: {
+        width:20,
+        height:20,
+        margin:10,
+      },
+      gambarProfil: {
+        width:70,
+        height:70,
+        borderRadius:80/2
+      },
+      textProfil: {
+        fontWeight:'bold',
+        fontSize:25,
+        color:'black'
+      },
+      keteranganProfil: {
+        fontSize:15,
+        color:'black'
+      },
+      textBio: {
+        fontSize:13,
+        color:'blue'
+      },
+      viewRow: {
+        backgroundColor:'white',
+        flex:1,
+        flexDirection:'row'
+      },
+      viewGambar: {
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+      },
+      gridGambar: {
+        width:100,
+        height:100,
+        borderColor:'black'
+      }
+    });
 
   export {style};
